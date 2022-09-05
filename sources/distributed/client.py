869ca9b1ffacaee3374530a0ef2c238786beccd0
masterdoors <lonelykeen@gmail.com>
2022-09-05 16:57:41 +0300

    Add files via upload

--- sources/distributed/client.py
+++ sources/distributed/client.py
@@ -23,7 +23,7 @@ import socket
 BUFFER_SIZE = 1024*32
 
 def command(cmd, id=-1, mask=None,addr=("localhost",5555)):
-    print(cmd,mark,addr)
+    print(cmd,mask,addr)
     package = [] 
     package.append(0)
     package.append(cmd)
