432bcdc0994a33e196754f6a84b6eac84b1d9b0d
masterdoors <lonelykeen@gmail.com>
2022-09-06 16:39:09 +0300

    Add files via upload

--- sources/distributed/client.py
+++ sources/distributed/client.py
@@ -581,5 +581,5 @@ def run_problem():
         return "Problem not found"            
 
 if __name__ == '__main__':
-     application.run(host='0.0.0.0')
+     application.run(host='0.0.0.0',port=80)
 
