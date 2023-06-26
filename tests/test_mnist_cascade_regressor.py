89da78854a4311ca9cde9b40ace84ee0b36c9a23
Keen <lonelykeen@gmail.com>
2023-06-26 12:36:19 +0000

    cascade regressor example

--- tests/test_mnist_cascade_regressor.py
+++ tests/test_mnist_cascade_regressor.py
@@ -11,14 +11,11 @@ y = np.sin(X).ravel()
 # add noise to targets
 y[::5] += 3 * (0.5 - np.random.rand(80))
 
-rf = RandomForestRegressor(max_depth=4)
+est = [RandomForestRegressor(max_depth=4) for _ in range(2)]
 
-est = [co2f.CO2ForestRegressor(C=3000, dual=False,tol = 0.001,max_iter=1000000,kernel='linear',\
-                                   max_depth=3,n_jobs=10,feature_ratio = 0.5,\
-                                   n_estimators=100) for i in range(int(2))]
-                                
 model = CascadeForestRegressor(max_layers=3)
 model.set_estimator(est)  
+
 
 lw = 2
 
@@ -27,9 +24,10 @@ model_color = ["m", "c", "g"]
 
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 10), sharey=True)
 
+model.fit(X, y)
 axes[0].plot(
     X,
-    rf.fit(X, y).predict(X),
+    model.predict(X),
     color=model_color[0],
     lw=lw,
     label="{} model".format(kernel_label[0]),
@@ -43,6 +41,14 @@ axes[0].scatter(
     s=50,
     label="other training data",
 )
+
+
+est = [co2f.CO2ForestRegressor(C=3000, dual=False,tol = 0.001,max_iter=1000000,kernel='linear',\
+                                   max_depth=3,n_jobs=10,feature_ratio = 0.5,\
+                                   n_estimators=100) for i in range(int(2))]
+                                
+model = CascadeForestRegressor(max_layers=3)
+model.set_estimator(est)  
 
 
 model.fit(X, y)
@@ -92,5 +98,5 @@ axes[2].scatter(
 
 fig.text(0.5, 0.04, "data", ha="center", va="center")
 fig.text(0.06, 0.5, "target", ha="center", va="center", rotation="vertical")
-fig.suptitle("Support Vector Regression", fontsize=14)
+fig.suptitle("Cascade (deep) Forest: RFR vs KFR vs refined KFR", fontsize=14)
 plt.show()
